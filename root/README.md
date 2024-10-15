611dca6c2a1e9b69de25d912f527509cc225dabe
Nathan Fletcher <nef111605@yahoo.com>
2024-10-15 15:22:46 -0500

    bug fix and add nix conf

---
+++ root/README.md
@@ -0,0 +1,2 @@
+# Root Folder
+This folder is treated like `/` in Linux. At the moment this is used to host my NixOS config file.
--- scripts/KickstartInstall.sh
+++
@@ -1,2 +0,0 @@
-sudo pacman -S git make unzip gcc ripgrep xclip
-git clone https://github.com/nvim-lua/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
